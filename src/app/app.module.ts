a4e25514c42eb31868bfd4a48e949754b206ec93
Zeroth <rjpt93@gmail.com>
2017-11-03 12:14:11 -0400

    Reportes creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DataEntryComponent } from './data-entry/data-entry.component';
+import { ReportesComponent } from './reportes/reportes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DataEntryComponent
+    DataEntryComponent,
+    ReportesComponent
   ],
   imports: [
     BrowserModule,
