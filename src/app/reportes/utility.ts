153fc7efdbd738ae261e8f02fe38a04fbcf50197
Zeroth <rjpt93@gmail.com>
2017-11-03 15:27:44 -0400

    Comment Utility and Solicitud

--- src/app/reportes/utility.ts
+++ src/app/reportes/utility.ts
@@ -1,50 +1,62 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-const url = "mongodb://admin:P455w0rd@practica00-shard-00-00-bn3aa.mongodb.net:27017,practica00-shard-00-01-bn3aa.mongodb.net:27017,practica00-shard-00-02-bn3aa.mongodb.net:27017/test?ssl=true&replicaSet=Practica00-shard-0&authSource=admin";
+// import { MongoClient } from 'mongodb';
+// const url = "mongodb://admin:P455w0rd@practica00-shard-00-00-bn3aa.mongodb.net:27017,practica00-shard-00-01-bn3aa.mongodb.net:27017,practica00-shard-00-02-bn3aa.mongodb.net:27017/test?ssl=true&replicaSet=Practica00-shard-0&authSource=admin";
 
-function obtenerReportesPorFecha(desde: Date, hasta: Date)
-{
-    try {
-        MongoClient.connect(url, function(err, db){
-            assert.equal(err, null);
-            db.collection('solicitudes').find({'created_at': {
-                $gte: hasta.toISOString(), $lte: desde.toISOString()
-            }}, (erro, result) => {
-                assert.equal(erro, null);
-                return result;
-            });
-        });
-    }catch {
-        console.log('Hubo un error');
-    }
-}
+// function obtenerReportesPorFecha(desde: Date, hasta: Date)
+// {
+//     try {
+//         MongoClient.connect(url, function(err, db){
+//             if(err) 
+//             {
+//                 throw err;
+//             }
+//             db.collection('solicitudes').find({'created_at': {
+//                 $gte: hasta.toISOString(), $lte: desde.toISOString()
+//             }}, 
+//             (erro, result) => {
+//                 if(erro) 
+//                 {
+//                     throw erro
+//                 };
+//                 return result;
+//             });
+//         });
+//     }catch {
+//         console.log('Hubo un error');
+//     }
+// }
 
-function obtenerPorDepartamento(departamento: string)
-{
-    try {
-        MongoClient.connect(url, function(err, db){
-            assert.equal(err, null);
-            db.collection('solicitudes').find({'departamento': departamento}, (erro, result) => {
-                assert.equal(erro, null);
-                return result;
-            });
-        });
-    }catch {
-        console.log('Hubo un error');
-    }
-}
+// function obtenerPorDepartamento(departamento: string)
+// {
+//     try {
+//         MongoClient.connect(url, function(err, db){
+//             if(err) throw err;
+//             db.collection('solicitudes').find({'departamento': departamento}, (erro, result) => {
+//                 if(erro) throw erro;
+//                 return result;
+//             });
+//         });
+//     }catch {
+//         console.log('Hubo un error');
+//     }
+// }
 
-function obtenerPorSolicitante(solicitante: string)
-{
-    try {
-        MongoClient.connect(url, function(err, db){
-            assert.equal(err, null);
-            db.collection('solicitudes').find({'solicitante': solicitante}, (erro, result) => {
-                assert.equal(erro, null);
-                return result;
-            });
-        });
-    } catch{
-        console.log('Hubo un error');
-    }
-}
+// function obtenerPorSolicitante(solicitante: string)
+// {
+//     try {
+//         MongoClient.connect(url, function(err, db){
+//             if(err)
+//             {
+//                 throw err;
+//             }
+//             db.collection('solicitudes').find({'solicitante': solicitante}, (erro, result) => {
+//                 if(erro)
+//                 {
+//                     throw erro;
+//                 }
+//                 return result;
+//             });
+//         });
+//     } catch{
+//         console.log('Hubo un error');
+//     }
+// }
