b5bef3b0bd7c3801d4916bb3524f3e52e4915721
Reynold Figueroa <reynold0801@gmail.com>
2017-11-03 12:53:32 -0400

    Routing
    Created Routes for home (menu), solicitud and default path (home)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
+import {
+    RouterModule,
+    Routes
+} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -9,18 +14,29 @@ import { MenuComponent } from './menu/menu.component';
 import { SolicitudComponent } from './solicitud/solicitud.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    MenuComponent,
-    SolicitudComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        MenuComponent,
+        SolicitudComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/**
+ * Routes are set here:
+ */
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: MenuComponent },
+    //{ path: 'archivar', component: Data-entryComponent },
+    //{ path: 'informes', component: InformesComponent }
+    { path: 'solicitud', component: SolicitudComponent }
+]
